84ce1693caa557c114ddb6e87dcfa0a186dbb359
HalkScout <andrewscouten@gmail.com>
2025-10-16 09:57:31 -0500

    Move code to ros2 package
    From tutorial: https://docs.ros.org/en/humble/Tutorials/Beginner-Client-Libraries/Creating-Your-First-ROS2-Package.html

---
+++ src/cli_teleop/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'cli_teleop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='yzb2',
+    maintainer_email='yzb2@txstate.edu',
+    description='Programming a Command Line Interface for complete teleoperation of Turtlebot3 with OMX Arm ',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
