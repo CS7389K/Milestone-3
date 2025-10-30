07a87ce5c9461ecb78ac086eb56a17f105d8cd74
Andrew Scouten <andrewscouten@gmail.com>
2025-10-30 10:52:31 -0500

    Change from humble to foxy
    Assignment requires foxy unfortunately, despite it being older.

--- install-ros2-foxy-desktop.sh
+++ install-ros2-foxy-desktop.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 ## Compiled script from various guides...
-# - ROS2 Humble:     https://docs.ros.org/en/humble/Installation/Ubuntu-Install-Debs.html
-# - Turtlebot3: 
-#   - Quick Start:   https://emanual.robotis.com/docs/en/platform/turtlebot3/quick-start/
-#   - Simulation:    https://emanual.robotis.com/docs/en/platform/turtlebot3/simulation/
-#   - Manipulation:  https://emanual.robotis.com/docs/en/platform/turtlebot3/manipulation/
+# - ROS2 foxy:  https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+# - Turtlebot3: https://web.archive.org/web/20240309202534/https://emanual.robotis.com/docs/en/platform/turtlebot3/manipulation/#turtlebot3-with-openmanipulator
 
 ## Ensure locale is UTF-8
 sudo apt update && sudo apt install locales -y
@@ -25,42 +22,42 @@ sudo dpkg -i /tmp/ros2-apt-source.deb
 ## Install Dependencies
 # ROS2 - Desktop
 sudo apt update && sudo apt upgrade -y
-sudo apt install ros-humble-desktop -y
+sudo apt install ros-foxy-desktop python3-argcomplete -y
 # ROS2 - Bare Bones
-# sudo apt install ros-humble-ros-base
+# sudo apt install ros-foxy-ros-base python3-argcomplete -y
 # ROS2 - Devtools
 sudo apt install ros-dev-tools -y
 # Activate ROS2 Environment
-source /opt/ros/humble/setup.bash
+source /opt/ros/foxy/setup.bash
 # TurtleBot3 Dependencies
 sudo apt install -y \
   python3-colcon-common-extensions \
-  ros-humble-gazebo-* \
-  ros-humble-cartographer \
-  ros-humble-cartographer-ros \
-  ros-humble-navigation2 \
-  ros-humble-nav2-bringup \
-  ros-humble-dynamixel-sdk \
-  ros-humble-ros2-control \
-  ros-humble-ros2-controllers \
-  ros-humble-gripper-controllers \
-  ros-humble-moveit*
+  ros-foxy-gazebo-* \
+  ros-foxy-cartographer \
+  ros-foxy-cartographer-ros \
+  ros-foxy-navigation2 \
+  ros-foxy-nav2-bringup \
+  ros-foxy-dynamixel-sdk \
+  ros-foxy-ros2-control \
+  ros-foxy-ros2-controllers \
+  ros-foxy-gripper-controllers \
+  ros-foxy-moveit
 # TurtleBot3 Modules
 mkdir -p ~/turtlebot3_ws/src
 cd ~/turtlebot3_ws/src/
-git clone -b humble  https://github.com/ROBOTIS-GIT/DynamixelSDK.git
-git clone -b humble  https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
-git clone -b humble  https://github.com/ROBOTIS-GIT/turtlebot3.git
-git clone -b humble  https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
-git clone -b humble  https://github.com/ROBOTIS-GIT/turtlebot3_manipulation.git
+git clone -b foxy https://github.com/ROBOTIS-GIT/DynamixelSDK.git
+git clone -b foxy https://github.com/ROBOTIS-GIT/turtlebot3_msgs.git
+git clone -b foxy https://github.com/ROBOTIS-GIT/turtlebot3.git
+git clone -b foxy https://github.com/ROBOTIS-GIT/turtlebot3_simulations.git
+git clone -b foxy https://github.com/ROBOTIS-GIT/turtlebot3_manipulation.git
 cd ~/turtlebot3_ws
 colcon build --symlink-install
 
 ## Environment
 echo 'export TURTLEBOT3_MODEL=burger' >> ~/.bashrc
 echo 'export ROS_DOMAIN_ID=30 #TURTLEBOT3' >> ~/.bashrc
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source /opt/ros/foxy/setup.bash' >> ~/.bashrc
 echo 'source ~/turtlebot3_ws/install/setup.bash' >> ~/.bashrc
 echo 'source /usr/share/gazebo/setup.sh' >> ~/.bashrc
-echo 'source /opt/ros/humble/setup.bash' >> ~/.bashrc
+echo 'source /opt/ros/foxy/setup.bash' >> ~/.bashrc
 source ~/.bashrc
